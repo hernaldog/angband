196bb852ff981c0e6437b1c8f04be487b6a0def5
Hernaldo González <hernaldog@gmail.com>
2026-06-26 10:55:56 -0400

    se traduce resto de archivo

--- src/cmd-misc.c
+++ src/cmd-misc.c
@@ -39,12 +39,12 @@ void do_cmd_wizard(void)
 	/* Verify first time */
 	if (!(player->noscore & NOSCORE_WIZARD)) {
 		/* Mention effects */
-		msg("You are about to enter 'wizard' mode for the very first time!");
-		msg("This is a form of cheating, and your game will not be scored!");
+		msg("¡Estás a punto de entrar en modo 'mago' por primera vez!");
+		msg("¡Esto es una forma de trampa, y tu partida no será puntuada!");
 		event_signal(EVENT_MESSAGE_FLUSH);
 
 		/* Verify request */
-		if (!get_check("Are you sure you want to enter wizard mode? "))
+		if (!get_check("¿Seguro que quieres entrar en modo mago? "))
 			return;
 
 		/* Mark savefile */
@@ -54,10 +54,10 @@ void do_cmd_wizard(void)
 	/* Toggle mode */
 	if (player->wizard) {
 		player->wizard = false;
-		msg("Wizard mode off.");
+		msg("Modo mago desactivado.");
 	} else {
 		player->wizard = true;
-		msg("Wizard mode on.");
+		msg("Modo mago activado.");
 	}
 
 	/* Update monsters */
@@ -74,7 +74,7 @@ void do_cmd_retire(struct command *cmd)
 {
 	/* Treat retired character as dead to satisfy end of game logic. */
 	player->is_dead = true;
-	my_strcpy(player->died_from, "Retiring", sizeof(player->died_from));
+	my_strcpy(player->died_from, "Retirándose", sizeof(player->died_from));
 }
 
 /**
@@ -94,19 +94,19 @@ void do_cmd_note(void)
 	my_strcpy(note, "", sizeof(note));
 
 	/* Read a line of input from the user */
-	if (!get_string("Note: ", tmp, sizeof(tmp))) return;
+	if (!get_string("Nota: ", tmp, sizeof(tmp))) return;
 
 	/* Ignore empty notes */
 	if (!tmp[0] || (tmp[0] == ' ')) return;
 
 	/* Format the note correctly, supporting some cute /me commands */
 	if (strncmp(tmp, "/say ", 5) == 0)
-		strnfmt(note, sizeof(note), "-- %s says: \"%s\"", player->full_name,
+		strnfmt(note, sizeof(note), "-- %s dice: \"%s\"", player->full_name,
 				&tmp[5]);
 	else if (strncmp(tmp, "/me", 3) == 0)
 		strnfmt(note, sizeof(note), "-- %s%s", player->full_name, &tmp[3]);
 	else
-		strnfmt(note, sizeof(note), "-- Note: %s", tmp);
+		strnfmt(note, sizeof(note), "-- Nota: %s", tmp);
 
 	/* Display the note (omitting the "-- " prefix) */
 	msg("%s", &note[3]);
